f5e5f0bb924fafbb452f9255c7850fb4c495c3f3
matsuge8 <a.tom.0215@gmail.com>
2024-04-19 12:44:53 +0900

    HowOld

---
+++ java-basic-1/src/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    public static void main(String[] args){
+
+        System.out.println("Hello!\"木村君\"\n");
+        System.out.print("A");
+        System.out.print("B");
+        System.out.print("C");
+    }
+
+}
